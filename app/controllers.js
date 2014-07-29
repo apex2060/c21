0276d22587dc5058a493a7d0fef6f6607a5a140d
Ryan Quinlan <apex2060@gmail.com>
2014-07-29 15:08:48 -0700

    Added Call Center

--- app/controllers.js
+++ app/controllers.js
@@ -55,81 +55,39 @@ var MainCtrl = app.controller('MainCtrl', function($rootScope, $scope, $routePar
 
 
 
-var ShoeBoxCtrl = app.controller('ShoeBoxCtrl', function($rootScope, $scope, $q, config, dataService, fileService, userService, qrService){
-	var myShoeBoxDefer = $q.defer();
+
+
+
+
+
+
+var CallCtrl = app.controller('CallCtrl', function($rootScope, $scope, $q, config, dataService){
+	var callListDefer = $q.defer();
 	userService.user().then(function(user){
-		var msb = new dataService.resource('shoeBox', user.objectId+'/shoeBoxList', true, true);
-			// msb.setQuery('where={"category":"apple"}');
-		myShoeBoxDefer.resolve(msb);
-		msb.item.list().then(function(data){
-			$scope.myShoeBoxes = data;
+		var calls = new dataService.resource('calls', user.objectId+'/calls', true, true);
+			// calls.setQuery('where={"shoeBox":"'+$rootScope.id+'"}');
+		callListDefer.resolve(calls);
+		calls.item.list().then(function(data){
+			$scope.calls = data;
 		})
-		$rootScope.$on(msb.listenId, function(event, data){
-			$scope.myShoeBoxes = data;
+		$rootScope.$on(calls.listenId, function(event, data){
+			$scope.calls = data;
 		})
 	});
-	var myShoeBoxPromise = myShoeBoxDefer.promise;
+	var callListPromise = callListDefer.promise;
 
 	var tools = {
-		qr: qrService,
-		initInd: function(){
-			myShoeBoxPromise.then(function(myShoeBoxes){
-				myShoeBoxes.item.get($rootScope.id).then(function(shoeBox){
-					console.log('Current Shoebox: ',shoeBox)
-					$scope.temp.currentShoeBox = shoeBox;
-				})
-			})
-		},
-		focus: function(shoeBox){
-			$rootScope.temp.currentShoeBox = shoeBox;
-		},
-		setPicture: function(details,src){
-			if(!$rootScope.temp.currentShoeBox)
-				$rootScope.temp.currentShoeBox = {};
-			$rootScope.$apply(function(){
-				$rootScope.temp.currentShoeBox.picture = {
-					temp: true,
-					status: 'uploading',
-					class: 'grayscale',
-					name: 'Image Uploading...',
-					src: src
-				};
-			})
-
-			fileService.upload(details,src,function(data){
-				$rootScope.$apply(function(){
-					$rootScope.temp.currentShoeBox.picture = {
-						name: data.name(),
-						src: data.url()
-					}
-				})
-			});
-		},
-		add: function(shoeBox){
-			myShoeBoxPromise.then(function(myShoeBoxes){
-				myShoeBoxes.item.save(shoeBox)
-			})
-			$scope.temp.shoeBox = {};
-		},
-		create: function(){
-			var shoeBox = {
-				title: 'Untitled ShoeBox',
-				location: 'Unknown Location'
-			}
-			ga('send', 'event', 'shoebox', 'create');
-			myShoeBoxPromise.then(function(myShoeBoxes){
-				myShoeBoxes.item.save(shoeBox).then(function(response){
-					window.location.hash = '#/shoebox/'+response.objectId
-				})
-			})
+		focusCall:function(callId){
+			
 		}
 	}
-	myShoeBoxPromise.then(function(myShoeBoxes){
-		it.myShoeBoxes = myShoeBoxes;
-		tools.shoeBox = myShoeBoxes.item;
+
+	callListPromise.then(function(myCalls){
+		it.myCalls = myCalls;
+		tools.item = myCalls.item;
 	})
 	$scope.tools = tools;
-	it.ShoeBoxCtrl=$scope;
+	it.CallCtrl=$scope;
 });
 
 
@@ -138,68 +96,6 @@ var ShoeBoxCtrl = app.controller('ShoeBoxCtrl', function($rootScope, $scope, $q,
 
 
 
-
-
-
-var ShoeBoxItemCtrl = app.controller('ShoeBoxItemCtrl', function($rootScope, $scope, $q, config, dataService, fileService, userService, qrService){
-	var myItemDefer = $q.defer();
-	userService.user().then(function(user){
-		var mitm = new dataService.resource('item', user.objectId+'/shoeBoxItems/'+$rootScope.id, true, true);
-			mitm.setQuery('where={"shoeBox":"'+$rootScope.id+'"}');
-			mitm.addLiveStream(user.objectId+'/allItems')
-		myItemDefer.resolve(mitm);
-		mitm.item.list().then(function(data){
-			$scope.myItems = data;
-		})
-		$rootScope.$on(mitm.listenId, function(event, data){
-			$scope.myItems = data;
-		})
-	});
-	var myItemPromise = myItemDefer.promise;
-
-	var tools = {
-		qr: qrService,
-		focus: function(item){
-			$rootScope.temp.currentItem = item;
-		},
-		remove: function(item, action){
-			if(!action){
-				$rootScope.temp.currentItem = item;
-				$('#itemRemoveModal').modal('show');
-			}else{
-				ga('send', 'event', 'item', 'remove', action);
-				if(action=='temp')
-					$rootScope.temp.currentItem.inBox = false;
-				else if(action == 'fromBox')
-					$rootScope.temp.currentItem.shoeBox = null;
-				else if(action == 'permanent')
-					tools.item.remove($rootScope.temp.currentItem)
-				$('#itemRemoveModal').modal('hide');
-			}
-		},
-		create: function(){
-			var item = {
-				title: 'Untitled Item',
-				shoeBox: $rootScope.id,
-				inBox: true
-			}
-			ga('send', 'event', 'item', 'create');
-			myItemPromise.then(function(myItems){
-				myItems.item.save(item).then(function(response){
-					// window.location.hash = '#/item/'+response.objectId
-					// alert('Item Created');
-					console.log('Item Created',response)
-				})
-			})
-		}
-	}
-	myItemPromise.then(function(myItems){
-		it.myItems = myItems;
-		tools.item = myItems.item;
-	})
-	$scope.tools = tools;
-	it.ShoeBoxItemCtrl=$scope;
-});
 
 
 
